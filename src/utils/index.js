549ade633235f81c545c7105222d4f28b20fd75a
PP <1041138537@qq.com>
2021-06-05 23:31:24 +0800

    调整日志记录

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,29 +2,6 @@ const fs = require('fs')
 const moment = require('moment')
 const path = require('path')
 
-function error(text) {
-    fs.appendFileSync(path.join(__dirname + '../../../log/error.log'), JSON.stringify({
-        date: moment().format('MMMM Do YYYY, h:mm:ss a'),
-        error: text
-    }, null, 2) + '\n')
-}
-
-function success(text) {
-    fs.appendFileSync(path.join(__dirname + '../../../log/successful.log'), JSON.stringify({
-        date: moment().format('MMMM Do YYYY, h:mm:ss a'),
-        text: text
-    }, null, 2) + '\n')
-}
-
-/**
- * 
- * @param {string} text 日志内容
- * @param {boolean} isError 是否为错误日志
- */
-function setLog(text, isError = false) {
-    const func = isError ? error : success
-    func(text)
-}
 
 /**
  * @description 更新彩虹屁词库
@@ -32,15 +9,15 @@ function setLog(text, isError = false) {
 function updateRainBowFartFile(text) {
     fs.appendFile(path.join(__dirname + '../../rainbow-fart'), '==>>>' + text + '\n', function (err) {
         if (err) {
-            setLog(err, true)
-        } else {
-            setLog('正常写入')
+            fs.appendFileSync(('error.log'), JSON.stringify({
+                date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                error: err
+            }, null, 2) + '\n')
         }
     })
 }
 
 module.exports = {
-    setLog,
     http: require('./http'),
     updateRainBowFartFile
 }
